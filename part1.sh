7c3892f4e75e78ad0802f755b6506d6b8d661e06
MKumaran9 <139612112+MKumaran9@users.noreply.github.com>
2023-07-29 10:01:28 +0530

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -2,10 +2,11 @@
 
 FILE=$1
 
+# find number of HTTP 200 errors
 echo "The number of HTTP 200 errors is "
 awk -F" " '{print $9}' $FILE | grep 200 | wc -l
 
-
+# find number of different HTTP response codes
 echo "List of HTTP response codes and their counts"
 awk -F" " '{print $9}' $FILE | sort | uniq -c
 
